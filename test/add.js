1a981e544a587c000dd7aee12eecd6caa0645c45
dwp <394308395@qq.com>
2018-03-09 17:55:05 +0800

    .

---
+++ test/add.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Eric on 2018/3/9.
+ */
+function add(x, y) {
+    return x + y;
+}
+
+module.exports = add;
---
+++ test/add.test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Eric on 2018/3/9.
+ */
+var add = require('./add.js');
+var expect = require('chai').expect;
+
+describe('加法函数的测试', function () {
+    it('1 加 1 应该等于 2', function () {
+        expect(add(1, 1)).to.be.equal(2);
+    });
+});
